f2ac2c6a4754715641c68068ec0127f07a9af26d
BoKeRou <li2583230913@gmail.com>
2020-08-10 17:40:53 +0800

    2020/8/10 First Commit

---
+++ app/src/main/java/com/bokerou/garbagecollection/GarbageCollectionApplication.kt
@@ -0,0 +1,17 @@
+package com.bokerou.garbagecollection
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class GarbageCollectionApplication : Application() {
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/com/bokerou/garbagecollection/ui/home/HomeFragment.kt
@@ -0,0 +1,2 @@
+package com.bokerou.garbagecollection.ui.home
+
